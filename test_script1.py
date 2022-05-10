de8fc1b4c2b90a1629d1613063934f6ef65e635e
Jalali-Lab @ UCLA <16159544+JalaliLabUCLA@users.noreply.github.com>
2022-05-09 18:20:22 -0700

    update test script

--- test_script1.py
+++ test_script1.py
@@ -17,28 +17,28 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # run PST CPU version
 pst = PST()
-pst_output = pst.run(img_file=img_file, phase_strength=0.3, warp_strength=12, sigma_LPF=0.1, thresh_min=-1, thresh_max=0.0019, morph_flag=1)
+pst_output = pst.run(img_file=img_file, phase_strength=0.3, warp_strength=15, sigma_LPF=0.15, thresh_min=-0.5, thresh_max=0.003, morph_flag=1)
 figure1 = plt.figure(1)
 plt.title('PST CPU')
 plt.imshow(pst_output, cmap='gray')
 
 # run PST GPU version
 pst = PST_GPU(device=device)
-pst_output = pst.run(img_file=img_file, phase_strength=0.3, warp_strength=12, sigma_LPF=0.1, thresh_min=-1, thresh_max=0.0019, morph_flag=1)
+pst_output = pst.run(img_file=img_file, phase_strength=0.3, warp_strength=15, sigma_LPF=0.15, thresh_min=-0.5, thresh_max=0.003, morph_flag=1)
 figure2 = plt.figure(2)
 plt.title('PST GPU')
 plt.imshow(pst_output.cpu().numpy(), cmap='gray')
 
 # run PAGE CPU version
 page = PAGE(direction_bins=10)
-page_edge = page.run(img_file=img_file, mu_1=0, mu_2=0.35, sigma_1=0.08, sigma_2=0.7, S1=0.3, S2=0.3, sigma_LPF=0.1, thresh_min=-1, thresh_max=0.0003, morph_flag=1)
+page_edge = page.run(img_file=img_file, mu_1=0, mu_2=0.35, sigma_1=0.05, sigma_2=0.7, S1=0.8, S2=0.8, sigma_LPF=0.08, thresh_min=-1, thresh_max=0.0004, morph_flag=1)
 figure3 = plt.figure(3)
 plt.title('PAGE CPU')
 plt.imshow(page_edge)
 
 # run PAGE GPU version
 page = PAGE_GPU(direction_bins=10, device=device)
-page_edge = page.run(img_file=img_file, mu_1=0, mu_2=0.35, sigma_1=0.08, sigma_2=0.7, S1=0.3, S2=0.3, sigma_LPF=0.1, thresh_min=-1, thresh_max=0.0003, morph_flag=1)
+page_edge = page.run(img_file=img_file, mu_1=0, mu_2=0.35, sigma_1=0.05, sigma_2=0.7, S1=0.8, S2=0.8, sigma_LPF=0.08, thresh_min=-1, thresh_max=0.0004, morph_flag=1)
 figure4 = plt.figure(4)
 plt.title('PAGE GPU')
 plt.imshow(page_edge.cpu().numpy())
